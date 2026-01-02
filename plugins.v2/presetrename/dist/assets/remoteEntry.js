31f73b5465e2c8e3d04b5556f43cd9309de776a9
qqcomeup <z1ljc5@gmail.com>
2026-01-03 01:10:04 +0800

    Add files via upload

--- plugins.v2/presetrename/dist/assets/remoteEntry.js
+++ plugins.v2/presetrename/dist/assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./Config":()=>{
-      dynamicLoadingCss(["style-LDaWYHNN.css"], false, './Config');
-      return __federation_import('./__federation_expose_Config-DqRoFrfD.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-Cyq9drxK.css"], false, './Config');
+      return __federation_import('./__federation_expose_Config-DRF-mIdt.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
